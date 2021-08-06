5a27eefd875bd4a285b99941018eacd523476704
nimmyja <nimmy.j.anoop@gmail.com>
2021-08-06 12:53:40 -0500

    Included comments to main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,17 +17,20 @@ if __name__ == '__main__':
     total_votes = 0
     with open(csv_path,'r') as csv_file:
         csv_reader =  csv.reader(csv_file,delimiter=',')
-        
-        csv_header = next(csv_reader)   #Skip the header row
-        cand_arr = []   # Store candidates name in a list
+        #Skip the header row
+        csv_header = next(csv_reader)   
+        # Store candidates name in a list
+        cand_arr = []   
         
         for row in csv_reader:
             total_votes +=1
             cand_name = row[2]
-            cand_arr.append(row[2])  
+            cand_arr.append(row[2]) 
 
+        #Identify unique candidate names from the candidate list
         unique_items = list(dict.fromkeys(cand_arr))    
 
+        #Printing Total number of votes
         print(f"Total Votes : {total_votes}")
         print_line()
         winner = 0
@@ -35,8 +38,10 @@ if __name__ == '__main__':
             cand_votes = cand_arr.count(candidate)            
             cand_percent ="{:.3%}".format(cand_votes/total_votes)
 
+            #Printing Total number and percentage of votes each candidate won
             print(f"{candidate} : {cand_percent} ({str(cand_votes)})")
             
+            #Finding winner of the election
             if winner < cand_votes:
                 winner = cand_votes
                 cand_won = candidate
@@ -44,6 +49,7 @@ if __name__ == '__main__':
         print(f"Winner : {cand_won}")
         print_line()
 
+#Writing ouput to text file
 output_path = os.path.join("Analysis","output.txt")
 with open(output_path,'w',newline='') as writer:
     writer.writelines("Election results")
